29ad6a8245aac9a4becafea41b85a170964b2b48
Ahmad Mansour <ahmad.mansour92@outlook.com>
2021-06-28 01:39:06 +0400

    initial Project

---
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+/**
+ * The [ViewModel] that is attached to the [OverviewFragment].
+ */
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData String that stores the status of the most recent request
+    private val _status = MutableLiveData<MarsApiStatus>()
+
+    // The external immutable LiveData for the request status String
+    val status: LiveData<MarsApiStatus>
+        get() = _status
+
+    private val _properties = MutableLiveData<List<MarsProperty>>()
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
+
+    /**
+     * Call getMarsRealEstateProperties() on init so we can display status immediately.
+     */
+    init {
+        getMarsRealEstateProperties()
+    }
+
+    /**
+     * Sets the value of the status LiveData to the Mars API status.
+     */
+    private fun getMarsRealEstateProperties() {
+        viewModelScope.launch {
+            try {
+                _status.value = MarsApiStatus.LOADING
+                val listResult = MarsApi.retrofitService.getProperties()
+                if (listResult.isNotEmpty()) {
+                    _properties.value = listResult
+                    _status.value = MarsApiStatus.DONE
+                }
+            } catch (exception: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
+            }
+        }
+    }
+}
